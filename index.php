b8278430e2838195dce5b78ce261fb61bef7d40e
musicmunky <musicmunky@gmail.com>
2015-05-31 21:04:22 -0400

    Adding code for first repo commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11-strict.dtd">
+<html>
+	<head>
+		<title>Swirl and Sip Wine</title>
+		<link rel="icon" type="image/icon" href="images/winelogo.ico">
+		<link rel='stylesheet' href='css/swirlandsipstyle.css' type="text/css" media="screen" charset="utf-8">
+		<link rel='stylesheet' href='css/fusionlib.css' type="text/css" media="screen" charset="utf-8">
+		<link rel='stylesheet' href='css/jquery-ui.min.css' type="text/css" media="screen" charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto">
+<!--		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans">//-->
+		<script language="javascript" type="text/javascript" src="javascript/jquery-1.11.0.min.js"></script>
+		<script language="javascript" type="text/javascript" src="javascript/jquery-ui-1.10.4.custom.min.js"></script>
+		<script language="javascript" type="text/javascript" src="javascript/fusionlib.js"></script>
+
+	</head>
+	<body>
+
+		<div id="header" class="header"></div>
+		<div id="mainwrapper" class="mainwrapper">
+			<div id="maincontent" class="maincontent">
+				Welcome!
+			</div>
+		</div>
+		<div class="footer"></div>
+
+	</body>
+</html>
